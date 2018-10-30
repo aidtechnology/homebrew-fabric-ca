87d6eb7dc9900085799198fcdd22f96a60f20d2f
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2018-10-30 05:09:29 +0200

    DO-213: Fix Fabric CA 1.3
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- Formula/fabric-ca@1.3.0.rb
+++ Formula/fabric-ca@1.3.0.rb
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-class FabricCaAT120 < Formula
+class FabricCaAT130 < Formula
   desc "Hyperledger Fabric CA native binaries installer"
   homepage "https://hyperledger.org/projects/fabric"
   url "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca/darwin-amd64-1.3.0/hyperledger-fabric-ca-darwin-amd64-1.3.0.tar.gz"
